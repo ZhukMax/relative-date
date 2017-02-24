9c6ef65d17e78bb9ff78b958f61d01e1d0bbe8a2
ZhukMax <zhukmax@ya.ru>
2017-02-24 20:56:29 +0300

    Fix class, add en_US.ini, edit README.md

--- README.md
+++ README.md
@@ -1,25 +1,21 @@
-# relative-date
-Relative date php
+# Relative date
+Relative date php-helper.
+
+## Install
+
+```console
+composer require zhukmax/relative-date
+```
 
 ## Usage
 
 ```php
 <?php
-include "relative-date.php";
 
-class Test {
-	use RelativeDateTrait;
-	
-	public function a()
-	{
-		$b = rand(-20, -1);
-		return $this->relativeDate(strtotime($b." week"));
-	}
-}
 $ru_Ru = array(
-	'только что',
-	'минуту назад',
-	'минут назад',
+	'',
+	'',
+	'',
 	'час назад',
 	'часов назад',
 	'вчера',
@@ -32,12 +28,7 @@ $ru_Ru = array(
 	'более года тому назад',
 	'лет тому назад'
 );
-$test = new Test;
-echo $str = $test->relativeDate(mktime(0, 0, 0, 2, 1, 2015), $ru_Ru);
-// => год назад
+echo $relativeDate = RelativeDate::get(mktime(0, 0, 0, 2, 1, 2015), $ru_Ru);
 
-echo $str2 = $test->relativeDate(mktime(0, 0, 0, 2, 1, 2016));
-// => 2 months ago
-
-echo $str3 = $test->a();
+echo $relativeDate2 = RelativeDate::get(mktime(0, 0, 0, 2, 1, 2015));
 ```
---
+++ src/RelativeDate.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Class RelativeDate
+ */
+class RelativeDate
+{
+    /**
+     * @var RelativeDate
+     */
+    private static $instance;
+
+    /**
+     * @var int
+     */
+	private $minute;
+
+    /**
+     * @var int
+     */
+	private $hour;
+
+    /**
+     * @var int
+     */
+	private $day;
+
+    /**
+     * @var int
+     */
+	private $week;
+
+    /**
+     * @var float|int
+     */
+    private $month;
+
+    /**
+     * @var int
+     */
+	private $year;
+
+    /**
+     * @var array
+     */
+	private $en_US;
+
+    /**
+     * RelativeDate constructor.
+     */
+	private function __construct()
+    {
+        $this->minute = 60;
+		$this->hour   = 60 * $this->minute;
+		$this->day    = 24 * $this->hour;
+		$this->week   = 7 * $this->day;
+		$this->year   = 365 * $this->day;
+		$this->month  = $this->year / 12;
+
+		$this->formats = array(
+			0,
+			$this->minute * 0.7,
+			$this->minute * 1.5,
+			$this->minute * 59.4,
+			$this->hour * 1.5,
+			$this->hour * 23,
+			$this->day * 2,
+			$this->day * 7,
+			$this->week * 1.5,
+			$this->week * 4,
+			$this->month * 1.5,
+			$this->month * 11.5,
+			$this->year * 1.2,
+			$this->year * 2
+		);
+		$this->en_US = parse_ini_file('en_US.ini');
+    }
+
+    /**
+     * Get instance of RelativeDate class,
+     * return result of function date.
+     *
+     * @param int $date
+     * @param array $lang
+     * @return mixed|string
+     */
+    public static function get($date, $lang = [])
+    {
+        if (is_null( self::$instance )) {
+            self::$instance = new self();
+        }
+
+        return self::$instance->date($date, self::$instance->getLang($lang));
+    }
+
+    private function getLang($lang){
+        if (!is_array($lang)) {
+            $lang = parse_ini_file($lang . '.ini');
+        }
+        if (count($lang) != count($this->en_US)) {
+            $lang = $this->en_US;
+        }
+
+        return $lang;
+    }
+
+    /**
+     * @param $date
+     * @param $lang
+     * @return mixed|string
+     */
+	private function date($date, $lang)
+	{
+		if (!$date) {
+		    return $lang[0];
+        }
+		$timeDifference = time() - (int)$date;
+		$output = $lang[0];
+
+		foreach ($this->formats as $key => $val) {
+			if ($timeDifference < (int)$val) break;
+			else $outKey = $key;
+		}
+
+		switch ($outKey) {
+			case 2:
+				$output = round($timeDifference / $this->minute) ." ". $lang[$outKey];
+				break;
+			case 4:
+				$output = round($timeDifference / $this->hour) ." ". $lang[$outKey];
+				break;
+			case 6:
+				$output = round($timeDifference / $this->day) ." ". $lang[$outKey];
+				break;
+			case 8:
+				$output = round($timeDifference / $this->week) ." ". $lang[$outKey];
+				break;
+			case 10:
+				$output = round($timeDifference / $this->month) ." ". $lang[$outKey];
+				break;
+			case 13:
+				$output = round($timeDifference / $this->year) ." ". $lang[$outKey];
+				break;
+		}
+
+		return $output;
+	}
+}
+?>
